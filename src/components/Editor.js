17481a01fb15d754a0b54034b05068b313b0913b
Tamal Anwar <tamalanwaronline@gmail.com>
2019-01-15 20:22:10 +0600

    Added renderMark function

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,10 +4,29 @@ import { Value } from 'slate';
 import initialValue from './_value';
 const value = Value.fromJSON(initialValue);
 
+function MarkHotkeys(options) {
+  let { key, type } = options;
+  return {
+    onKeyDown(event, editor, next) {
+      if (!event.ctrlKey || event.key !== key) return next();
+      event.preventDefault();
+      editor.toggleMark(type);
+    }
+  };
+}
+
+const plugins = [
+  MarkHotkeys({ key: 'b', type: 'bold' }),
+  MarkHotkeys({ key: 'i', type: 'italic' }),
+  MarkHotkeys({ key: 'u', type: 'underline' }),
+  MarkHotkeys({ key: '-', type: 'strikethrough' }),
+  MarkHotkeys({ key: '`', type: 'code' })
+];
+
 /**
  * Main app component file
  */
-export default class Editor extends Component {
+export default class MyEditor extends Component {
   state = {
     value
   };
@@ -18,6 +37,25 @@ export default class Editor extends Component {
 
   ref = (editor) => (this.editor = editor);
 
+  // Render Mark Function
+  renderMark = (props, editor, next) => {
+    let { mark, attributes, children } = props;
+    switch (mark.type) {
+      case 'bold':
+        return <strong {...attributes}>{children}</strong>;
+      case 'italic':
+        return <em {...attributes}>{children}</em>;
+      case 'underline':
+        return <u {...attributes}>{children}</u>;
+      case 'code':
+        return <code {...attributes}>{children}</code>;
+      case 'strikethrough':
+        return <del {...attributes}>{children}</del>;
+      default:
+        return next();
+    }
+  };
+
   render() {
     return (
       <div id="shell">
@@ -27,6 +65,8 @@ export default class Editor extends Component {
           placeholder="Start writing.."
           value={this.state.value}
           onChange={this.onChange}
+          plugins={plugins}
+          renderMark={this.renderMark}
         />
       </div>
     );
